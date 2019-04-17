da35ef80b9980c59a4749e9ee517b7b44a8d172c
zy <502372@qq.com>
2019-04-17 22:31:34 +0800

    init

---
+++ tutorial/04input.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+'''
+    python2中，input()函数中，等待用户输入内容，作为代码执行
+    python2中, raw_input()函数，等价于python3中的input()函数
+
+    python3中，input()函数中，等待用户输入内容，作为字符串处理
+
+'''
+
+
+s = input('something:')
+print(s)
+
+
+# python2中 执行04input.py
+# 输入1+1,打印2
+
+# python3中 执行04input.py
+# 输入1+2,打印1+1
+
+# python3中没有改函数，只有python2中有，改函数等价于python3中的input()
+# s = raw_input('something:')
+# print(s)
